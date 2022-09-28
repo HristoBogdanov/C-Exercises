91771ca0195a7d6451c8545aff102f321cccb93a
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-09-28 15:06:07 +0300

    Add files via upload

---
+++ console_abstract_question/console_abstract_question/IntegerQuestion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_abstract_question
+{
+    public class IntegerQuestion : Question
+    {
+        private int studentanswer;
+        private int wrighanswer;
+        public int StudentAnswer {
+            get
+            {
+                return this.studentanswer;
+            }
+            set {
+                if (value <= 0) throw new ArgumentException("Invalid data");
+                else this.studentanswer = value;
+            }
+            }    
+        public int WrightAnswer
+        {
+            get {
+                return this.wrighanswer;
+            }
+            set {
+                if (value <= 0) throw new ArgumentException("Invalid data");
+                else this.wrighanswer = value;
+            }
+        }
+
+        public IntegerQuestion(int studentanswer, int wrightanswer)
+        {
+            this.StudentAnswer = studentanswer;
+            this.WrightAnswer = wrighanswer;
+        }
+
+        public override void askQuestion()
+        {
+            Console.WriteLine("Question: What is 4 times 5? Answer on next line: ");
+            this.StudentAnswer = int.Parse(Console.ReadLine());
+            
+        }
+    }
+}
---
+++ console_abstract_question/console_abstract_question/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_abstract_question
+{
+    public abstract class Question
+    {
+        public abstract void askQuestion();
+        public abstract void checkQuestion();
+
+    }
+}
