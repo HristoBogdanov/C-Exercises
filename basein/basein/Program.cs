37ebe47ddf8d76e1439d31213595eaf70d9eee7a
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-10-17 16:41:06 +0300

    Add files via upload

---
+++ basein/basein/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basein
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert R and H with 1 interval in between:");
+            string st = Console.ReadLine();
+            var pom = st.Split(' ').ToList();
+
+            Console.WriteLine("Insert price for 1 cubic meter and tax/100 with one interval space in between:");
+            string st1 = Console.ReadLine();
+            var pom1 = st1.Split(' ').ToList();
+
+            pool p = new pool(double.Parse(pom[0]), double.Parse(pom[1]));
+            Console.WriteLine($"The volume of the pool is {Math.Round(p.V(),2)}");
+            Console.WriteLine($"The price for water is {Math.Round(p.Price_water(double.Parse(pom1[0]),double.Parse(pom[1])),2)}lv.");
+        }
+    }
+}
---
+++ basein/basein/pool.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basein
+{
+    public class pool
+    {
+        private double r;
+        private double h;
+
+        public double R
+        {
+            get { return this.r; }
+            set { this.r = value; }
+        }
+
+        public double H
+        {
+            get { return this.h; }
+            set { this.h = value; }
+        }
+
+
+        public pool(double r,double h)
+        {
+            this.R = r;
+            this.H = h;
+        }
+        public double V()
+        {
+            return Math.PI * this.R * this.R * this.H;
+        }
+        public double Price_water(double price, double tax)
+        {
+            return this.V() * price * (1 + tax / 100);
+        }
+    }
+}
