a8993c57883f7532f5206769e49d26b1c4b72cfd
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-10-17 16:41:40 +0300

    Add files via upload

---
+++ Box/Box/Box_all.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Box
+{
+    public class Box_all<T>
+    {
+        private List<T> spisuk;
+        public int Count { get { return spisuk.Count; } }
+        public List<T> Spisuk
+        {
+            get { return this.spisuk; }
+            set{ this.spisuk = value; }
+        }
+        public Box_all()
+        {
+            this.spisuk = new List<T>();
+        }
+        public void AddBox(T element)
+        {
+            spisuk.Insert(0, element);
+        }
+        
+        public T Remove()
+        {
+            T first = spisuk.First();
+            spisuk.RemoveAt(0);
+            return first;
+        }
+        
+        public override string ToString()
+        {
+            string result="";
+            foreach (T x in spisuk)
+            { 
+                result = $"{result} - {x}";
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Box/Box/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Box
+{
+    public partial class Form1 : Form
+    {
+        Box_all<string> secret = new Box_all<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string treasure = textBox1.Text;
+            string sweet = comboBox1.Text;
+            if (treasure != "")
+            {
+                secret.AddBox(treasure);
+                textBox1.Clear();
+            }
+            else
+            {
+                secret.AddBox(sweet);
+                comboBox1.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(secret.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {          
+            listBox1.Items.Clear();
+            secret.Remove();
+            listBox1.Items.Add(secret.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add($"Брой предмети в списъка: {secret.Count}");
+        }
+    }
+}
