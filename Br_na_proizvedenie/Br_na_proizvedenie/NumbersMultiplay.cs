882971f7ee0e345fbdfae293943b47a1dd888d23
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-10-19 21:42:03 +0300

    Add files via upload

---
+++ Br_na_proizvedenie/Br_na_proizvedenie/NumbersMultiplay.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Br_na_proizvedenie
+{
+    
+    public class NumbersMultiplay
+    {
+        List<int> cifri_na_chislo = new List<int>();
+        private int cifri;
+        public int Cifri
+        {
+            get { return this.cifri; }
+            set { this.cifri = value; }
+        }
+        public NumbersMultiplay(string cifri)
+        {
+            this.cifri = Cifri;
+        }
+        public List<int> GetResult()
+        {
+            do
+            {
+                int p = 0;
+                p = Cifri % 10;
+                cifri_na_chislo.Add(p);
+                Cifri = Cifri / 10;
+            } while (Cifri > 0);
+            return cifri_na_chislo;
+        }
+
+    }
+}
