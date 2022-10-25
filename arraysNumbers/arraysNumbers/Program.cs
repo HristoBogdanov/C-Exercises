218b5de5a71bf81758f69266d54d6595514d3046
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-10-25 09:00:06 +0300

    Add files via upload

---
+++ arraysNumbers/arraysNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arraysNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arrThree = new List<int>();
+
+            List<int> arrOne = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> arrTwo = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            for (int i = 0; i < arrOne.Count; i++)
+            {
+                for (int j = 0; j < arrTwo.Count; j++)
+                {
+                    if (arrOne[i] == arrTwo[j]) arrThree.Add(arrTwo[j]);
+                }
+            }
+            Console.WriteLine(string.Join(" ",arrThree));
+        }
+    }
+}
