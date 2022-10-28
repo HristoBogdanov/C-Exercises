3255e729ce5a3f00afd936520c4705d3fae99710
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-10-28 21:42:57 +0300

    Add files via upload

---
+++ detska_gradina_priem/detska_gradina_priem/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace detska_gradina_priem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 26;
+            this.listBox1.Location = new System.Drawing.Point(172, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(442, 156);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 63);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Изведи всички деца";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::detska_gradina_priem.Properties.Resources.Background_Form;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(627, 650);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Детска градина \"Усмивка\"";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ detska_gradina_priem/detska_gradina_priem/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace detska_gradina_priem
+{
+    public partial class Form1 : Form
+    {
+        StreamReader reader = new StreamReader("children.txt", Encoding.UTF8);
+        StreamWriter writer = new StreamWriter("accepted_children.txt",false,Encoding.UTF8);
+        string name,age,adress,nomer,childGardenAdress, hasBrotherOrSister;
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Указания за записване на дете :\n 1. Въведете името на детето. \n 2. Въведете годините му с цифра. \n 3. Въведете адреса му. \n 4. Въведете неговия номер. \n 5.Въведете адреса на детската градина, в която искате да бъде записано.\n 6.Напишете с една дума дали детето има брат или сестра в тази детска градина. \n Всяка информация се пише на нов ред ! \n \n Пример: \n Христо Богданов \n 5 \n кв.Левски \n 12 \n кв.Левски \n да \n");
+            
+        }
+
+        public Form1()
+        {   
+        InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            name = reader.ReadLine();
+            age = reader.ReadLine();
+            adress = reader.ReadLine();
+            nomer = reader.ReadLine();
+            childGardenAdress = reader.ReadLine();
+            hasBrotherOrSister = reader.ReadLine();
+            while(name!=null && age!=null && adress!=null && nomer!=null && childGardenAdress!=null)
+            {
+                       if(int.Parse(age)>=3 && adress==childGardenAdress && hasBrotherOrSister=="да" && nomer!=null)
+                {
+                    listBox1.Items.Add(name + " е приет/та!");
+                    writer.WriteLine(name);
+                    writer.WriteLine(age);
+                }
+                else listBox1.Items.Add(name + " не е приет/та");
+                name = reader.ReadLine();
+                age = reader.ReadLine();
+                adress = reader.ReadLine();
+                nomer = reader.ReadLine();
+                childGardenAdress = reader.ReadLine();
+                hasBrotherOrSister = reader.ReadLine();
+            }
+            reader.Close();
+            writer.Close();
+    }   
+    }
+}
+    
+    
+    
+
