3dac4706e1dd3b61849876026a15fa3703dd5b02
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-04 22:39:54 +0200

    Add files via upload

---
+++ KSI - 2015_2/KSI - 2015_2/Deal.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSI___2015_2
+{
+    public class Deal
+    {
+
+
+        public int Day
+        {
+            get; set;
+        }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public int Hour{ get; set; }
+        public int Minutes { get; set; }
+        public int Code { get; set; }
+        public int Quality { get; set; }
+        public int Quantity { get; set; }
+        public string Name_deliverer { get; set; }
+
+        public Deal(int year, int month, int day,int hour,int minutes, int code,int quality,int quantity,string name)
+        {
+            this.Day = day;
+            this.Month = month;
+            this.Year = year;
+            this.Hour = hour;
+            this.Minutes = minutes;
+            this.Code = code;
+            this.Quality = quality;
+            this.Quantity = quantity;
+            this.Name_deliverer = name;
+        }
+        public string Code_word()
+        {
+            string result = "";
+            if (this.Code == 1) result = "манатарка";
+            else if (this.Code == 2) result = "печурка";
+            else if (this.Code == 3) result = "кладница";
+            else if (this.Code == 4) result = "пачи крак";
+            else if (this.Code == 5) result = "сърнела";
+            else if (this.Code == 6) result = "друг вид";
+
+            return result;
+        }
+        public string Quality_word()
+        {
+            string quality_word="";
+            if (this.Quality == 1) quality_word = "1-во";
+            else if (this.Quality == 2) quality_word = "2-ро";
+            else if (this.Quality == 3) quality_word = "3-то";
+
+            return quality_word;
+        }
+        public override string ToString()
+        {
+            return $"{this.Day}.{this.Month}.{this.Year} {this.Hour}:{this.Minutes}, {this.Code_word()}, {this.Quantity:f2} кг., {this.Quality_word()}, {this.Name_deliverer}";
+        }
+    }
+}
---
+++ KSI - 2015_2/KSI - 2015_2/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KSI___2015_2
+{
+    public partial class Form1 : Form
+    {
+        List<Deal> deals = new List<Deal>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+            int N = int.Parse(textBox6.Text);
+            if (N > 5000) throw new ArgumentException("Надвишен брой сделки!");
+            else 
+            {
+                int year = int.Parse(string.Join("", input[0], input[1], input[2], input[3]));
+                int month= int.Parse(string.Join("", input[4], input[5]));
+                int day= int.Parse(string.Join("", input[6], input[7]));
+                int hour= int.Parse(string.Join("", input[8], input[9]));
+                int minutes = int.Parse(string.Join("", input[10], input[11]));
+                int code = int.Parse(textBox2.Text);
+                int quality = int.Parse(textBox3.Text);
+                int quantity = int.Parse(textBox4.Text);
+                string name_worker = textBox5.Text;
+
+                Deal deal = new Deal(year, month, day, hour, minutes, code, quality, quantity, name_worker);
+                deals.Add(deal);
+
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (Deal x in deals) listBox1.Items.Add(x.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var sorted_deals_2 = deals.OrderBy(x => x.Quality).ThenByDescending(x => x.Quantity);
+
+            foreach (Deal x in sorted_deals_2) listBox1.Items.Add(x.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int[] Kilos = new int[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                foreach (Deal x in deals) if (x.Code == i) Kilos[i] += x.Quantity;
+            }
+
+            var sorted_Kilos = Kilos.OrderBy(x => Kilos);
+
+        }
+    }
+}
