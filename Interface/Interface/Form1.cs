72a77ffc08aa5d6cb42aca2826cd082850f76f72
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-04 22:38:13 +0200

    Add files via upload

---
+++ Interface/Interface/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IContainer container = new ACMEContainer();
+            ACMEContainer container1 = new ACMEContainer();
+            container.setContainerID(1001);
+            container1.setContainerID(1002);
+            label1.Text = container.GetContainerID().ToString();
+            label2.Text = container1.GetContainerID().ToString();
+
+        }
+    }
+}
---
+++ Interface/Interface/IContainer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface IContainer
+    {
+        int GetContainerID();
+        int GetContainerWeight();
+        void setContainerID(int id);
+    }
+
+    public class ACMEContainer:IContainer
+        {
+        private int id;
+        private int weight;
+        public int GetContainerID()
+        {
+            return this.id;
+        }
+
+        public int GetContainerWeight()
+        {
+            return this.weight;
+        }
+        public void setContainerID(int id)
+        {
+            this.id = id;
+        }
+        }
+}
+
