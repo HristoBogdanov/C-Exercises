f41ec3e440cd6088790db248e4d416057935b4d4
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-09 21:22:09 +0200

    Add files via upload

---
+++ Library/Library/Library/Autor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Autor
+    {
+        private string name;
+        private string lastname;
+        private int birth_year;
+        private string death_year;
+        private List<Book> books;
+
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int Birth_year { get; set; }
+        public string Death_year { get; set; }
+        public List<Book> Books  { get; set; }
+        public Autor(string name, string lastname, int birth_year, string death_year)
+        {
+            this.Name = name;
+            this.Lastname = lastname;
+            this.Birth_year = birth_year;
+            this.Death_year = death_year;
+            
+        }
+        public string AliveorDeath()
+        {
+            if (this.Death_year=="") return "-";
+            else return this.Death_year;
+        }
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+    }
+}
---
+++ Library/Library/Library/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public enum Janr
+    {
+        Kriminalen,
+        Treilur,
+        Poeziq,
+        Fantastika,
+        Detski,
+        Prikliuchenski
+    }
+    public class Book
+    {
+        private string name;
+        private Janr janr;
+        private string kat_nomer;
+
+        public string Name { get; set; }
+        public Janr Janr { get; set; }
+        public string Kat_nomer { get; set; }
+        public Book(string name, Janr janr, string kat_nomer)
+            {
+            this.Name = name;
+            this.Janr = janr;
+            this.Kat_nomer = kat_nomer;
+            }         
+    }
+}
