ed78f437ced8946a4b22d78c5c4fa93ad5d47ba9
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-09 21:21:15 +0200

    Add files via upload

---
+++ KSI_2017_2/KSI_2017_2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KSI_2017_2
+{
+    public partial class Form1 : Form
+    {
+        List<Worker> workers = new List<Worker>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sorted_workers_2 = workers.OrderBy(x => x.Country).ThenBy(x => x.Name_worker);
+
+            foreach (Worker x in sorted_workers_2) listBox1.Items.Add(x.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "") throw new ArgumentException("Missing data");
+            Worker worker = new Worker(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text, textBox6.Text);
+            workers.Add(worker);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<Worker> workers_empty = new List<Worker>();
+
+            foreach (Worker x in workers) if (x.City == "" || x.Postcode == "") workers_empty.Add(x);
+
+            var sorted_list_3 = workers_empty.OrderBy(x => x.EGN);
+            foreach (Worker x in sorted_list_3) listBox1.Items.Add(x.ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            foreach (Worker x in workers)
+            {
+                if (x.Print_post() != "Missing data") listBox1.Items.Add(x.Print_post());
+            }
+        }
+    }
+}
---
+++ KSI_2017_2/KSI_2017_2/Worker.cs
@@ -0,0 +1,117 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSI_2017_2
+{
+    public class Worker
+    {
+        private string name_worker;
+        private string egn;
+        private string name_worker_latin;
+        private string country;
+        private string city;
+        private string postcode;
+
+        public string Name_worker
+        {
+            get { return this.name_worker; }
+            set {
+                if (value.Length > 50) throw new ArgumentException("Incorrect data!");
+                else this.name_worker = value;
+            }
+        }
+
+        public string EGN
+        {
+            get { return this.egn; }
+            set {
+                if (value.Length > 15) throw new ArgumentException("Incorrect data!");
+                else this.egn = value;
+            }
+        }
+
+        public string Name_worker_latin
+        {
+            get { return this.name_worker_latin; }
+            set {
+                if (value.Length > 50) throw new ArgumentException("Incorrect data!");
+                else this.name_worker_latin = value;
+            }
+        }
+
+        public string Country
+        {
+            get { return this.country; }
+            set {
+                if (value.Length > 30) throw new ArgumentException("Incorrect data!");
+                else this.country = value;
+            }
+        }
+
+        public string City
+        {
+            get { return this.city; }
+            set {
+                if (value.Length > 30) throw new ArgumentException("Incorrect data!");
+                else this.city = value;
+            }
+        }
+
+        public string Postcode
+        {
+            get { return this.postcode; }
+            set {
+                if (value.Length > 30) throw new ArgumentException("Incorrect data!");
+                else this.postcode = value;
+            }
+        }
+
+        public Worker(string name,string egn,string name_latin,string country,string city, string postcode)
+        {
+            this.Name_worker = name;
+            this.EGN = egn;
+            this.Name_worker_latin = name_latin;
+            this.Country = country;
+            this.City = city;
+            this.Postcode = postcode;
+        }
+        public Worker(string name,string egn,string name_latin,string country)
+        {
+            this.Name_worker = name;
+            this.EGN = egn;
+            this.Name_worker_latin = name_latin;
+            this.Country = country;
+        }
+
+        public Worker(string name, string egn, string country)
+        {
+            this.Name_worker = name;
+            this.EGN = egn;
+            this.Country = country;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name_worker}, {this.EGN}, {this.Name_worker_latin}, {this.Country}, {this.City}, {this.Postcode}";
+        }
+        public string Print_post()
+        {
+            string result = "";
+            if (this.Name_worker_latin != "")
+            {
+                var names = this.Name_worker_latin.Split(' ').ToArray();
+                if (names.Length == 1) result = $"{names[0]}@nncomputers.com";
+                else if (names.Length == 2) result = $"{names[1]}_{names[0]}@nncomputers.com";
+                else if (names.Length == 3) result = $"{names[2]}_{names[0]}_{names[1].First()}@nncomputers.com";
+
+            }
+            else result = "Missing data";
+
+            return result;
+        }
+    }
+}
