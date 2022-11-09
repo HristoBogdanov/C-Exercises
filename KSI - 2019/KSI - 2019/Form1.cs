d06caf1004cf9e597b2d0b75067838db2a762e24
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-09 21:20:10 +0200

    Add files via upload

---
+++ KSI - 2019/KSI - 2019/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KSI___2019
+{
+    public partial class Form1 : Form
+    {
+        List<Star> stars = new List<Star>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name_star = textBox1.Text;
+            double razstoqnie = double.Parse(textBox2.Text);
+            int type = int.Parse(textBox3.Text);
+            double mass = double.Parse(textBox4.Text);
+            string suzvezdie = textBox5.Text;
+
+            Star star = new Star(name_star, razstoqnie, type, mass, suzvezdie);
+            stars.Add(star);
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var sorted_stars = stars.OrderBy(x => x.Length);
+
+            foreach (Star x in sorted_stars) listBox1.Items.Add(x.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var sorted_stars_2 = stars.OrderBy(x => x.Alignment).OrderByDescending(x => x.Mass);
+
+            foreach (Star x in sorted_stars_2) listBox2.Items.Add(x.ToString());
+        }
+    }
+}
---
+++ KSI - 2019/KSI - 2019/Star.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSI___2019
+{
+    public class Star
+    {
+        private string name_star;
+        private double length;
+        private int type;
+        private double mass;
+        private string alignment;
+
+        public string Name_star
+        {
+            get { return this.name_star; }
+            set {
+                if (value.Length > 20) throw new ArgumentException("Incorrect data!");
+                else this.name_star = value;
+            }
+        }
+        public double Length
+        {
+            get { return this.length; }
+            set {
+                if (value < 0) throw new ArgumentException("Incorrect data!");
+                else this.length = value;
+            }
+        }
+
+        public int Type
+        {
+            get { return this.type; }
+            set {
+                if (value < 0 || value > 9) throw new ArgumentException("Incorrect data!");
+                else this.type = value;
+            }
+        }
+
+        public double Mass
+        {
+            get { return this.mass; }
+            set
+            {
+                if (value < 0) throw new ArgumentException("Incorrect data!");
+                else this.mass = value;
+            }
+        }
+        
+        public string Alignment
+        {
+            get { return this.alignment; }
+            set {
+                if (value.Length > 30) throw new ArgumentException("Incorrect data!");
+                else this.alignment = value;
+            }
+        }
+
+        public Star(string name_star,double length,int type,double mass,string alignment)
+        {
+            this.Name_star = name_star;
+            this.Length = length;
+            this.Type = type;
+            this.Mass = mass;
+            this.Alignment = alignment;
+        }
+        public string Type_string()
+        {
+            string result;
+            int type_pom = this.Type;
+            switch (type_pom)
+            {
+                case 1: result = "хипергигант";
+                    break;
+                case 2:
+                    result = "свръхгигант";
+                    break;
+                case 3:
+                    result = "ярък гигант";
+                    break;
+                case 4:
+                    result = "гигант";
+                    break;
+                case 5:
+                    result = "субгигант";
+                    break;
+                case 6:
+                    result = "джудже";
+                    break;
+                case 7:
+                    result = "субджудже";
+                    break;
+                case 8:
+                    result = "червено джудже";
+                    break;
+                case 9:
+                    result = "кафяво джудже";
+                    break;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.Name_star}, {this.Length} св.г., {this.Type_string()}, {this.Mass} сл.м., {this.Alignment}";
+        }
+    }
+}
