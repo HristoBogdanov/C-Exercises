1714a8031933eb363d62c739638b085aa3e471dc
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-13 11:01:06 +0200

    Add files via upload

---
+++ Line/Line/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Line
+{
+    public partial class Form1 : Form
+    {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Random r = new Random();
+          
+           
+            Graphics G = e.Graphics;
+            G.Clear(Color.Azure);
+            
+            for (int i = 0; i < 100000000; i++)
+            {
+                Pen p = new Pen(Color.FromArgb(r.Next(1, 256), r.Next(1, 256), r.Next(1, 256)), 5);
+                Point a = new Point(r.Next(0, 500), r.Next(0, 500));
+                Point b = new Point(r.Next(0, 500), r.Next(0, 500));
+                G.DrawLine(p, a, b);
+                System.Threading.Thread.Sleep(1);
+            }
+           
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
