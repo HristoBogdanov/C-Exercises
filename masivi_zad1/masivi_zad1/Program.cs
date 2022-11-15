c2a908ba045bcd7a266a7756a5eb4e7064a88187
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-15 21:42:08 +0200

    Add files via upload

---
+++ masivi_zad1/masivi_zad1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masivi_zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>();
+
+            string input = Console.ReadLine();
+
+            while(input!="end")
+            {
+                if (words.Contains(input)) Console.WriteLine("The word is already in! Enter something else!");
+                else words.Add(input);
+
+                input = Console.ReadLine();
+            }
+
+            words = words.OrderByDescending(q => q).ToList();
+
+            foreach (string x in words) Console.WriteLine(x);
+        }
+    }
+}
