471ebd1e42a12d7f318fdd554b2987d15ede1b45
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-15 21:42:36 +0200

    Add files via upload

---
+++ masivi_zad2/masivi_zad2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace masivi_zad2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader("input.txt", Encoding.UTF8);
+            List<number> numbers = new List<number>();
+            string input = reader.ReadLine();
+
+            var pom = input.Split(' ').ToList();
+            
+            for(int i=0;i<pom.Count;i++)
+            {
+                number n = new number(int.Parse(pom[i]));
+                numbers.Add(n);
+            }
+            
+            for(int i=0;i<numbers.Count;i++)
+            {
+                for(int j=0;j<numbers.Count-1;j++)
+                {
+                    if (numbers[j].GetSum(0) < numbers[j + 1].GetSum(0))
+                    {
+                        int pomm = numbers[j].Stoinost;
+                        numbers[j].Stoinost = numbers[j + 1].Stoinost;
+                        numbers[j+1].Stoinost=pomm;
+                    }
+                }
+            }
+            foreach (number x in numbers) listBox1.Items.Add(x.Stoinost);
+            
+        }
+    }
+}
---
+++ masivi_zad2/masivi_zad2/number.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masivi_zad2
+{
+    public class number
+    {
+       public int Stoinost { get; set; }
+
+        public number(int stoinost)
+        {
+            this.Stoinost = stoinost;
+        }
+
+        public int GetSum(int sum)
+        {
+            int stoinost = this.Stoinost;
+            do
+            {
+                sum+=  stoinost % 10;
+                stoinost = stoinost / 10;
+            } while (stoinost != 0);
+            return sum;
+        }
+    }
+}
