5d149f96d5c77028f3743a6e0f455c6c3cc1b104
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-16 13:07:00 +0200

    Add files via upload

---
+++ masivi_zad6/masivi_zad6/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace masivi_zad6
+{
+    public partial class Form1 : Form
+    {
+        List<Student> students = new List<Student>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student student = new Student(textBox1.Text, int.Parse(textBox2.Text), int.Parse(textBox3.Text), int.Parse(textBox4.Text));
+            students.Add(student);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for(int i=0;i<students.Count();i++)
+            {
+                for(int j=i+1;j<students.Count();j++)
+                    if(students[i].Score()<students[j].Score())
+                    {
+                        string pom_name = students[i].Name;
+                        int pom_result1 = students[i].Result1;
+                        int pom_result2 = students[i].Result2;
+                        int pom_result3 = students[i].Result3;
+
+                        students[i].Name = students[j].Name;
+                        students[i].Result1 = students[j].Result1;
+                        students[i].Result2 = students[j].Result2;
+                        students[i].Result3 = students[j].Result3;
+
+                        students[j].Name = pom_name;
+                        students[j].Result1 = pom_result1;
+                        students[j].Result2 = pom_result2;
+                        students[j].Result3 = pom_result3;
+                    }
+            }
+            foreach (Student x in students) listBox1.Items.Add(x.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var result1 = students.OrderByDescending(x => x.Result1);
+
+            listBox1.Items.Add(result1.First().ToString());
+
+            var result2 = students.OrderByDescending(x => x.Result2);
+
+            listBox1.Items.Add(result2.First().ToString());
+
+            var result3 = students.OrderByDescending(x => x.Result3);
+
+            listBox1.Items.Add(result3.First().ToString());
+        }
+    }
+}
---
+++ masivi_zad6/masivi_zad6/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masivi_zad6
+{
+    class Student
+    {
+        private string name;
+        private int result1;
+        private int result2;
+        private int result3;
+
+        public string Name {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int Result1
+        {
+            get { return this.result1; }
+            set { this.result1 = value; }
+        }
+
+        public int Result2
+        {
+            get { return this.result2; }
+            set { this.result2 = value; }
+        }
+
+        public int Result3
+        {
+            get { return this.result3; }
+            set { this.result3 = value; }
+        }
+
+        public Student(string name,int result1,int result2,int result3)
+        {
+            this.Name = name;
+            this.Result1 = result1;
+            this.Result2 = result2;
+            this.Result3 = result3;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} -- {this.Result1} -- {this.Result2} -- {this.result3}";
+        }
+
+        public int Score()
+        {
+            return Result1 + Result2 + Result3;
+        }
+    }
+}
