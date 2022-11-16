bff5166b929f8748440968ed972e4423e8d4f512
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-16 13:06:16 +0200

    Add files via upload

---
+++ masivi_zad5/masivi_zad5/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace masivi_zad5
+{
+    public partial class Form1 : Form
+    {
+        List<River> rivers = new List<River>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            River river = new River(textBox1.Text, int.Parse(textBox2.Text));
+            rivers.Add(river);
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for(int i=0;i<rivers.Count();i++)
+            {
+                for(int j=0;j<rivers.Count()-1;j++)
+                {
+                    if(rivers[j].Length<rivers[j+1].Length)
+                    {
+                        string pom_name = rivers[j].Name;
+                        int pom_length = rivers[j].Length;
+
+                        rivers[j].Name = rivers[j + 1].Name;
+                        rivers[j].Length = rivers[j + 1].Length;
+
+                        rivers[j + 1].Name = pom_name;
+                        rivers[j + 1].Length = pom_length;
+                    }
+                }
+            }
+            foreach (River x in rivers) listBox1.Items.Add(x.ToString());
+        }
+    }
+}
---
+++ masivi_zad5/masivi_zad5/River.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masivi_zad5
+{
+    public class River
+    {
+        public string Name { get; set; }
+        public int Length { get; set; }
+
+        public River(string name,int length)
+        {
+            this.Name = name;
+            this.Length = length;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} - {this.Length}";
+        }
+    }
+}
