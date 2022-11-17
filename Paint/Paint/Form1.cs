7965cb6e9d17d6302b2785123c0f6c48f860ca3e
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-17 18:46:55 +0200

    Add files via upload

---
+++ Paint/Paint/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Random r = new Random();
+
+
+            Graphics g = e.Graphics;
+
+            for (int i = 1; i < 1000000; i++)
+            {
+                g.Clear(Color.FromArgb(r.Next(0, 256), r.Next(0, 256), r.Next(0, 256)));
+                System.Threading.Thread.Sleep(300);
+            }
+            
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
