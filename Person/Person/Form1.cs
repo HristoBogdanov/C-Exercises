17cc4c9ba3bc68edfd2e75d07bbfe1d1a9fd2aa0
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-19 18:08:35 +0200

    Add files via upload

---
+++ Person/Person/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Person
+{
+    public partial class Form1 : Form
+    {
+        List<Student> students = new List<Student>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student student = new Student(textBox1.Text,textBox2.Text,int.Parse(textBox3.Text));
+            students.Add(student);
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (Student x in students)
+                listBox1.Items.Add(x.ToString());
+        }
+    }
+}
---
+++ Person/Person/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+   public interface Person
+    {
+        string Name
+        { get; set; }
+        string LastName
+        { get; set; }
+        int BirthDate
+        { get; set; }
+
+        
+    }
+    public class Student : Person
+    {
+        private string name;
+        private string lastname;
+        private int birthDate;
+        
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value;}
+        }
+        public string LastName
+        {
+            get { return this.lastname; }
+            set { this.lastname = value; }
+        }
+        public int BirthDate
+        {
+            get { return this.birthDate; }
+            set { this.birthDate = value; }
+        }
+        public Student(string name, string lastname, int birthDate)
+        {
+            this.Name = name;
+            this.LastName = lastname;
+            this.BirthDate = birthDate;
+        }
+        public string Age()
+        {
+            return (2021 - BirthDate).ToString();
+        }
+        public string IsHeStudent()
+        {
+            int Age = 2021 - BirthDate;
+            if (Age >= 7 && Age <= 19) return "Ученик";
+            else return "Не е ученик";
+        }
+        public string ToString()
+        {
+            return $"{this.Name} {this.LastName} - {this.BirthDate}г. - {this.Age()} години - {this.IsHeStudent()}";
+        }
+    }
+}
