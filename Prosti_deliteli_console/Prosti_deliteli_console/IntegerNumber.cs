379cbd46877d94f2893f934a1a1f3abc39eff6fb
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-21 14:08:12 +0200

    Add files via upload

---
+++ Prosti_deliteli_console/Prosti_deliteli_console/IntegerNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prosti_deliteli_console
+{
+    class IntegerNumber
+    {
+        private int stoinost;
+        public int Stoinost
+        {
+            get { return this.stoinost; }
+            set { this.stoinost = value; }
+        }
+        public IntegerNumber(int a)
+        {
+            Stoinost = a;
+        }
+
+        public string Razlagane()
+        {
+            
+            int pom = this.stoinost;
+            List<int> result = new List<int>();
+            for (int i = 2; i <=pom; i++)
+            {
+                while (pom % i == 0)
+                {
+                    pom = pom / 2;
+                    result.Add(i);
+                }
+               
+            }
+            string st = string.Join(" ", result);
+            return st;
+        }
+    }
+}
---
+++ Prosti_deliteli_console/Prosti_deliteli_console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prosti_deliteli_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            IntegerNumber number = new IntegerNumber(a);
+
+            Console.WriteLine(number.Razlagane());
+        }
+    }
+}
