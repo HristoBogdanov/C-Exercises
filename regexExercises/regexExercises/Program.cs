876c431f323ad5f6142369df630d271fec0b9911
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-23 14:15:11 +0200

    Add files via upload

---
+++ regexExercises/regexExercises/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace regexExercises
+{
+    class Program
+    {
+       public static bool AlmostOnlyLetters(string word)
+        {
+            Regex pattern = new Regex(@"^[0-9 a-zA-z\.]+");
+            Match match = pattern.Match(word);
+
+            return match.Success;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AlmostOnlyLetters(Console.ReadLine()));      
+        }
+    }
+}
