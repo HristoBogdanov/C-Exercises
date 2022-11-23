e4ab4da07819b4a07212638bb2cf6a2d4a92eda3
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-23 14:16:24 +0200

    Add files via upload

---
+++ reverse_string/reverse_string/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string st = Console.ReadLine();
+            string st_reverse;
+            List<char> characters = new List<char>();
+
+            foreach (char x in st) characters.Add(x);
+
+            characters.Reverse();
+            st_reverse = String.Join("", characters);
+            Console.WriteLine(st_reverse);
+        }
+    }
+}
