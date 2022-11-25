e6fdaeb60ffd0c8c8764c75cc2907d21a91f7372
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-25 08:56:22 +0200

    Add files via upload

---
+++ Stack_basic/Stack_basic/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stack_basic
+{
+    public partial class Form1 : Form
+    {
+        Places place = new Places(50);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            do
+            {
+                listBox1.Items.Add(place.Pop());
+            }
+            while (place.Size() > 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            place.Push(textBox1.Text);
+            textBox1.Clear();
+        }
+    }
+}
---
+++ Stack_basic/Stack_basic/Places.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_basic
+{
+   public class Places
+    {
+        private int n;
+        private int top;
+        private string[] Elements = new string[1];
+
+        public Places(int m)
+        {
+            this.n = m;
+            Array.Resize(ref this.Elements, n);
+            top = -1;
+        }
+        public void Push(string x)
+        {
+            if (this.top == n - 1)
+            {
+                n = n + 32;
+                Array.Resize(ref this.Elements, n);
+            }
+
+             top++;
+                Elements[top] = x;
+            
+                    
+            
+        }
+        public string Pop()
+        {
+            if (top == -1)
+                throw new Exception("Error");
+            return this.Elements[top--];
+        }
+        public string Look()
+        {
+            if (top == -1)
+                throw new Exception("Error");
+            return this.Elements[top];
+        }
+        public int Size()
+        {
+            int k = top;
+            k++;
+            return k;
+        }
+
+    }
+}
