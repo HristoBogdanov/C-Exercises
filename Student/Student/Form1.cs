ba2e70d900579b8796796a03cd5dab41a585f1d5
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-26 10:02:19 +0200

    Add files via upload

---
+++ Student/Student/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Student
+{
+    public partial class Form1 : Form
+    {
+        List<int> grades = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student student = new Student(textBox1.Text, textBox2.Text);
+            grades = textBox3.Text.Split(',').Select(int.Parse).ToList();
+            foreach (int x in grades) if (x < 2 || x > 6) throw new ArgumentException("Въведете оценка от 2 до 6");
+
+             listBox1.Items.Add(student.Min(grades));
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Student student = new Student(textBox1.Text, textBox2.Text);
+            grades = textBox3.Text.Split(',').Select(int.Parse).ToList();
+            foreach (int x in grades) if (x < 2 || x > 6) throw new ArgumentException("Въведете оценка от 2 до 6");
+            listBox1.Items.Add(student.Max(grades));
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Student student = new Student(textBox1.Text, textBox2.Text);
+            grades = textBox3.Text.Split(',').Select(int.Parse).ToList();
+            foreach (int x in grades) if (x < 2 || x > 6) throw new ArgumentException("Въведете оценка от 2 до 6");
+            listBox1.Items.Add(student.Average(grades));
+        }
+    }
+}
---
+++ Student/Student/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    public class Student
+    {
+        private string name;
+        private string discipline;
+        public string Name
+        {
+            get { return this.name; }
+            set {this.name = value; }
+        }
+        public string Discipline
+        {
+            get { return this.discipline; }
+            set { this.discipline = value; }
+        }
+        public Student(string name, string discipline)
+        {
+            this.Name = name;
+            this.Discipline = discipline;
+        }
+        
+        public int Min(List<int> a)
+        {
+            int min = a[0];
+            for (int i = 1; i < a.Count; i++)
+            {
+                if (a[i] < min) min=a[i];
+            }
+            return min;
+            
+        }
+        public int Max(List<int> a)
+        {
+            int max = a[0];
+            for (int i = 1; i < a.Count; i++)
+            {
+                if (a[i] > max) max=a[i];
+            }
+            return max;
+        }
+        public double Average(List<int> a)
+        {
+            double sum=0;
+            foreach (int x in a) sum = sum + x;
+            double average = sum / a.Count;
+            return average;
+        }
+    }
+}
