550e71b1d0cf7e5ee44475b1a71d60a74181373a
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-11-28 22:00:09 +0200

    Add files via upload

---
+++ Tail/Tail/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tail
+{
+    public partial class Form1 : Form
+    {
+        public class intQueue
+        {
+            public int n;
+            public int bottom, top;
+            public int[] Elemets = new int[1];
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            queue intqueue = new queue(int.Parse(textBox2.Text));
+            intqueue.Push(int.Parse(textBox1.Text));
+            textBox1.Clear();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            queue intqueue = new queue(int.Parse(textBox2.Text));
+            listBox1.Items.Add(intqueue.Output());
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            queue intqueue = new queue(int.Parse(textBox2.Text));
+            listBox1.Items.Add(intqueue.Look());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            queue intqueue = new queue(int.Parse(textBox2.Text));
+            listBox1.Items.Add(intqueue.Count());
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            queue intqueue = new queue(int.Parse(textBox2.Text));
+            listBox1.Items.Add(intqueue.Pop());
+        }
+    }
+}
---
+++ Tail/Tail/queue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tail
+{
+    public class queue
+    {
+        private int n;
+        private int bottom, top;
+        private int[] Elemets = new int[1];
+
+        public queue(int m)
+        {
+            this.n = m;
+            Array.Resize(ref this.Elemets, n);
+            top = 0;
+            bottom = -1;
+        }
+        public void Push(int x)
+        {
+            bottom = bottom + 1;
+            this.Elemets[bottom] = x;
+        }
+        public int Pop()
+        {
+            return Elemets[top++];
+        }
+        public int Look()
+        {
+            return this.Elemets[top];
+        }
+        public int Count()
+        {
+            return bottom - top + 1;
+        }
+        public int[] Output()
+        {
+            return Elemets;
+        }
+    }
+}
