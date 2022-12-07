fa95af796e1e8f4d7535f8c5ed9b0257d177dd1f
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-12-07 17:56:33 +0200

    Add files via upload

---
+++ Матура/Employee_Console/Employee_Console/Employee.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Console
+{
+    public class Employee
+    {
+        private string name_e;
+        private double salary;
+        private string job;
+        private string area;
+        private string email;
+        private int age;
+
+        public string Name_e
+        {
+            get { return this.name_e; }
+            set { if (value == null) throw new ArgumentException("Invalid input");
+                else this.name_e = value;
+            }
+        }
+
+        public double Salary
+        {
+            get { return this.salary; }
+            set {
+                if (value == 0) throw new ArgumentException("Invalid input");
+                else this.salary = value;
+            }
+        }
+
+        public string Job
+        {
+            get { return this.job; }
+            set
+            {
+                if (value == null) throw new ArgumentException("Invalid input");
+                else this.job = value;
+            }
+        }
+
+        public string Area
+        {
+            get { return this.area; }
+            set {
+                if (value == null) throw new ArgumentException("Invalid input");
+                else this.area = value;
+            }
+        }
+
+        public string Email
+        {
+            get { return this.email; }
+            set { this.email = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public Employee(string name_e,double salary,string job,string area,string email,int age)
+        {
+            this.Name_e = name_e;
+            this.Salary = salary;
+            this.Job = job;
+            this.Area = area;
+            this.Email = email;
+            this.Age = age;
+        }
+        public string Print()
+        {
+            return $"{this.Name_e} -- {this.Salary} -- {this.Email} -- {this.Age}";
+        }
+    }
+}
---
+++ Матура/Employee_Console/Employee_Console/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            double salary;
+            string job;
+            string area;
+            string email;
+            int age;
+            List<Employee> employees = new List<Employee>();
+            string input = Console.ReadLine();
+
+            do
+            {
+                var pom = input.Split(' ').ToArray();
+                name = pom[0];
+                salary = int.Parse(pom[1]);
+                job = pom[2];
+                area = pom[3];
+                if (pom[4] != "") email = pom[4];
+                else email = "n/a";
+
+                if (pom[5] != "") age = int.Parse(pom[5]);
+                else age = -1;
+                Employee employee = new Employee(name, salary, job, area, email, age);
+                employees.Add(employee);
+
+                input = Console.ReadLine();
+            }
+            while (input != null);
+
+            foreach (Employee x in employees) Console.WriteLine(x.Print());
+        }
+    }
+}
