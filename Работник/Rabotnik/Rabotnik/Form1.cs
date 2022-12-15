737f4d229638c9dae4e9f8becf32d9e6e7f2586b
Hristo Bogdanov <97587030+HristoBogdanov@users.noreply.github.com>
2022-12-16 00:04:27 +0200

    Add files via upload

---
+++ Работник/Rabotnik/Rabotnik/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rabotnik
+{
+    public partial class Form1 : Form
+    {
+        List<Rabotnik> rabotnici = new List<Rabotnik>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Rabotnik rabotnik = new Rabotnik() ;
+            rabotnik.ime = textBox1.Text;
+            rabotnik.rabota = textBox2.Text;
+            rabotnik.hours = double.Parse(textBox3.Text);
+            rabotnik.pari = int.Parse(textBox4.Text);
+            rabotnici.Add(rabotnik);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            foreach (Rabotnik x in rabotnici)
+                listBox1.Items.Add(x.Print());
+        }
+    }
+}
---
+++ Работник/Rabotnik/Rabotnik/Rabotnik.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabotnik
+{
+    public class Rabotnik
+    {
+        public string ime;
+        public string rabota;
+        public double hours;
+        public int pari;
+
+        public double zaplata() {
+            return hours * pari;
+        }
+        public double Hours {
+            get
+            {
+                return this.hours;
+
+            }
+            set {
+                if (hours < 0)
+                    throw new ArgumentException("Часовете не могат да бъдат отрицателно число");
+                this.hours = value;
+            }
+        }
+        public int Pari {
+            get {
+                return this.pari;
+            }
+            set {
+                if (pari < 10)
+                    throw new ArgumentException("Заплащането не може да бъде по-малко от 10лв.");
+                this.pari = value;
+            }
+        }
+        public string Print() {
+            return $"{this.ime} - {this.rabota} - {zaplata()}";
+        }
+    }
+}
